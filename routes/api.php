e9298a3fbebb74f555f4e53aab8faa84d5721f5b
waheilee <waheilee@gmail.com>
2020-09-06 22:46:34 +0800

    解决跨域问题

--- routes/api.php
+++ routes/api.php
@@ -15,9 +15,8 @@ use \Illuminate\Support\Facades\Route;
 Route::middleware('cors')->group(function () {
 
     Route::post('merchant_info','Api\MerchantApi@addMerchant');//商家信息提交
-    Route::post('upload_photo',function () {
-        return '正常访问123';
-    });//上传图片
+    Route::post('upload','Api\MerchantApi@photo');//上传图片
+
 });
 
 
