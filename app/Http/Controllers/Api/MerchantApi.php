5013180a84e0a092a2bdeb14e74405286d58560f
waheilee <waheilee@gmail.com>
2020-09-23 19:31:33 +0800

    解决跨域问题

--- app/Http/Controllers/Api/MerchantApi.php
+++ app/Http/Controllers/Api/MerchantApi.php
@@ -66,7 +66,7 @@ class MerchantApi extends Controller
 
     public function test(Request $request)
     {
-        return $request->all();
+        return 'success';
         try{
             $data = $this->merchantService->setPhoto($request);
             return $this->wrapSuccessReturn(compact('data'));
