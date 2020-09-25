357d0d8c4286f94e1fb6ed3ec33137a3a950fe0e
waheilee <waheilee@gmail.com>
2020-09-25 16:43:11 +0800

    test

--- app/Models/UploadsModel.php
+++ app/Models/UploadsModel.php
@@ -39,8 +39,8 @@ class UploadsModel {
 
 //构造类，file:上传文件域
     function __construct($file, $path = "") {
-        return 123;
         $this->file_name = $_FILES[$file]['name']; //客户端原文件名
+        return $this->file_name;
         $this->file_type = $_FILES[$file]['type']; //文件类型
         $this->file_tem = $_FILES[$file]['tmp_name']; //储存的临时文件名，一般是系统默认
         $this->file_size = $_FILES[$file]['size']; //文件大小
