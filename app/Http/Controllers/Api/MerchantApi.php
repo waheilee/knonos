dfd037560bfa52cf9647d1de7687ea8ea84e288f
waheilee <waheilee@gmail.com>
2020-09-25 15:11:48 +0800

    test

--- app/Http/Controllers/Api/MerchantApi.php
+++ app/Http/Controllers/Api/MerchantApi.php
@@ -61,7 +61,7 @@ class MerchantApi extends Controller
     public function test(Request $request)
     {
             $file = $request->file('logo');
-            return $file;
+
             if($file->isValid()){
                 $ext = $file->getClientOriginalExtension();//文件扩展名
                 $file_name = date("YmdHis",time()).'-'.uniqid().".".$ext;//保存的文件名
