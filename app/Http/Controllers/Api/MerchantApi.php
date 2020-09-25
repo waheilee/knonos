e0abe7be7cb8537fc446b5f2f785dddd37d40c5c
waheilee <waheilee@gmail.com>
2020-09-25 15:14:18 +0800

    test

--- app/Http/Controllers/Api/MerchantApi.php
+++ app/Http/Controllers/Api/MerchantApi.php
@@ -65,6 +65,7 @@ class MerchantApi extends Controller
 
             if($file->isValid()){
                 $ext = $file->getClientOriginalExtension();//文件扩展名
+                return $ext;
                 $file_name = date("YmdHis",time()).'-'.uniqid().".".$ext;//保存的文件名
                 if(!in_array($ext,['jpg','jpeg','gif','png']) ) return response()->json('文件类型不是图片');
                 //把临时文件移动到指定的位置，并重命名
